a76918dbf5f1b661b249a2c9f5d5fa867c765a4e
flashfan88 <lavarjohnson20@gmail.com>
2019-09-19 11:58:17 -0500

    webpage

---
+++ MyWebpage/webpage.js
@@ -0,0 +1,9 @@
+
+this_button = document.getElementById("topbutton");
+
+
+// When the user clicks on the button, scroll to top of page
+function topFunction() {
+
+    document.documentElement.scrollTop = 0;
+}
